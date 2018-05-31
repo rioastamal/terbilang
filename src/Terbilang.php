a8c26a3d901ceb9263ccc2e95f4c03d901d5f20c
Rio Astamal <me@rioastamal.net>
2018-05-31 07:35:29 +0700

    Inittial commit

---
+++ src/Terbilang.php
@@ -0,0 +1,289 @@
+<?php
+/**
+ * Class untuk menterjemahkan angka ke bilangan dalam Bahasa Indonesia.
+ * Sebagai contoh '48' akan diterjemahkan menjadi 'empat puluh delapan'
+ * dan seterusnya. Bilangan yang diterjemahkan class ini hanya sampai
+ * pada penggunaan satuan triliun. Jadi 1000 triliun tidak diubah menjadi
+ * 1 kuadriliun.
+ *
+ * @author      Rio Astamal <rio@rioastamal.net>
+ * @copyright   2018 Rio Astamal <rio@rioastamal.net>
+ * @category    Math
+ * @license     MIT License <https://opensource.org/licenses/MIT>
+ */
+namespace RioAstamal\AngkaTerbilang;
+
+class Terbilang
+{
+    /**
+     * Pemisah angka desimal dibelakang koma.
+     */
+    protected $pemisahDesimal;
+
+    /**
+     * Array bilangan dari mulai satu hingga sebelas. Nol tidak
+     * dimasukkan karena perlu perlakuan special.
+     *
+     * @var array
+     */
+    protected $bilangan = [
+            '',
+            'satu',
+            'dua',
+            'tiga',
+            'empat',
+            'lima',
+            'enam',
+            'tujuh',
+            'delapan',
+            'sembilan',
+            'sepuluh',
+            'sebelas'
+    ];
+
+    /**
+     * Class constructor.
+     *
+     * @param string $pemisahDesimal
+     * @return void
+     */
+    public function __construct($pemisahDesimal='.')
+    {
+        $this->pemisahDesimal = $pemisahDesimal;
+    }
+
+    /**
+     * Static constructor untuk membuat object tanpa new keyword.
+     *
+     * @param string $pemisahDesimal
+     * @return RioAstamal\AngkaTerbilang\Terbilang
+     */
+    public static function create($pemisahDesimal='.')
+    {
+        return new static($pemisahDesimal);
+    }
+
+    /**
+     * Method wrapper yang mengkombinasikan terjemahan angka bulat
+     * dan desimal.
+     *
+     * @param float $angka Angka yang akan diterjemahkan
+     * @return string
+     */
+    public function terbilang($angka)
+    {
+        // Hapus semua karakter yang bukan angka dan yang digunakan
+        // oleh scientific notation seperti e,E,+ contoh 2.5E+2
+        $angka = preg_replace('/([^0-9eE\+' . $this->pemisahDesimal . '])/', '', $angka);
+        $angka = str_replace($this->pemisahDesimal, '.', $angka);
+
+        if (! is_numeric($angka)) {
+            throw new TerbilangException('ERROR: Angka tidak dapat dikenali');
+        }
+
+        if (strpos((string)$angka, '.') === false) {
+            if ($angka + 0 === 0) {
+                return 'nol';
+            }
+            return $this->terjemahkanAngka($angka);
+        }
+
+        list($angka, $desimal) = explode('.', $angka, 2);
+
+        if ($angka + 0 === 0) {
+            return 'nol koma ' . $this->terjemahkanPerAngka($desimal);
+        }
+
+        return rtrim($this->terjemahkanAngka($angka) . ' koma ' . $this->terjemahkanPerAngka($desimal));
+    }
+
+    /**
+     * @param float $angka Angka yang akan diterjemahkan
+     * @param string
+     */
+    protected function terjemahkanAngka($angka)
+    {
+        $angkaAsli = $angka;
+
+        // pastikan kita hanya berususan dengan tipe data numeric
+        $angka = (float)$angka;
+
+        // Angka dibawah 12 dapat langsung dimapping ke index array $bilangan
+        if ($angka < 12) {
+            return $this->bilangan[$angka];
+        }
+
+        // Angka belasan didapat dengan cara pengurangan angka tersebut
+        // dengan 10. Hasilnya langsung dapat dimapping ke index array $this->bilangan
+        // dan ditambahkan suffix ' belas'.
+        //
+        // Contoh:
+        // 1. 18 - 10 = 8 -> index ke-8 + 'belas'
+        // 2. 17 - 10 = 7 -> index ke-7 + 'belas'
+        if ($angka < 20) {
+            return $this->bilangan[$angka - 10] . ' belas';
+        }
+
+        // Angka puluhan didapat dengan dua operasi yaitu pembagian dan
+        // modulus dengan angka 10. Hasil bagi dan modulus masing-masing
+        // akan dimapping ke index array $this->bilangan
+        //
+        // Contoh angka 48:
+        // 48 / 10 = 4.8 => dibulatkan -> 4 -> index ke-4 + 'puluh'
+        // 48 % 10 = 8 -> index ke-8
+        if ($angka < 100) {
+            $hasilBagi = floor($angka / 10);
+            $hasilMod = $angka % 10;
+
+            return rtrim(sprintf('%s puluh %s',
+                $this->bilangan[$hasilBagi],
+                $this->bilangan[$hasilMod]
+            ));
+        }
+
+        // Angka seratusan didapat dengan mengurangkan angka tersebut
+        // dengan 100. Hasil dari pengurangan tersebut dapat berupa
+        // satuan dan puluhan oleh karenanya kita gunakan rekursif
+        // untuk mendapat bilangan tersebut.
+        //
+        // Contoh 100:
+        // 100 - 100 = 0 -> 'seratus ' + index ke-0
+        //
+        // Contoh 125:
+        // 125 - 100 = 25 -> 'seratus ' + terjemahkanAngka(25)
+        if ($angka < 200) {
+            return rtrim(sprintf('seratus %s', static::terjemahkanAngka($angka - 100)));
+        }
+
+        // Angka ratusan didapat mirip dengan cara mendapatkan angka puluhan.
+        // Perbedaannya pada ratusan kita menggunakan rekursif untuk sisa
+        // modulusnya
+        //
+        // Contoh 205:
+        // 205 / 100 = 2.05 => dibulatkan -> 2 -> index ke-2 + ' ratus'
+        // 205 % 100 = 5 -> terjemahkanAngka(5)
+        //
+        // Contoh 499:
+        // 499 / 100 = 4.99 => dibulatkan -> 4 -> index ke-4 + ' ratus'
+        // 499 % 100 = 99 -> terjemahkanAngka(99)
+        if ($angka < 1000) {
+            $hasilBagi = floor($angka / 100);
+            $hasilMod = $angka % 100;
+
+            return rtrim(sprintf('%s ratus %s',
+                $this->bilangan[$hasilBagi],
+                static::terjemahkanAngka($hasilMod)
+            ));
+        }
+
+        // Angka seribuan.
+        //
+        // Contoh 1011:
+        // 1011 - 1000 = 11 => 'seribu ' + terjemahkanAngka(11)
+        if ($angka < 2000) {
+            return rtrim(sprintf('seribu %s', static::terjemahkanAngka($angka - 1000)));
+        }
+
+        // Flag
+        $tidakTeridentifikasi = false;
+
+        // Angka ribuan sampai ratusan ribu
+        if ($angka < 1000000) {
+            $hasilBagi = floor($angka / 1000);
+            $hasilMod = $angka % 1000;
+
+            return rtrim(sprintf('%s ribu %s',
+                static::terjemahkanAngka($hasilBagi),
+                static::terjemahkanAngka($hasilMod)
+            ));
+        }
+
+        // Angka jutaan sampai ratusan juta (dibawah 1 Milyar)
+        if ($angka < 1000000000) {
+            $hasilBagi = floor($angka / 1000000);
+            $hasilMod = $angka % 1000000;
+
+            return rtrim(sprintf('%s juta %s',
+                static::terjemahkanAngka($hasilBagi),
+                static::terjemahkanAngka($hasilMod)
+            ));
+        }
+
+        // BC Math tidak mensupport notasi scientific sepert 1.0E+5
+        // Jadi perlu diubah menjadi normal string number.
+        // Disini kita juga akan menghilangkan angka decimal dibelakang koma
+        $angkaAsli = explode('.', sprintf('%f', $angkaAsli))[0];
+
+        // Angka milyaran sampai ratusan milyar (dibawah 1 Triliun)
+        // Karena angka cukup besar dan sistem 32 bit hanya sampai pada
+        // kisaran 2 Milyar, maka digunakan extension BC Math.
+        if (bccomp($angkaAsli, '1000000000000') === -1) {
+            $hasilBagi = floor(bcdiv($angkaAsli, '1000000000'));
+            $hasilMod = bcmod($angkaAsli, '1000000000');
+
+            return rtrim(sprintf('%s milyar %s',
+                static::terjemahkanAngka($hasilBagi),
+                static::terjemahkanAngka($hasilMod)
+            ));
+        }
+
+        // Angka triliunan. Angka diatas 1000 triliun tidak diubah
+        // ke bentuk satuan lain seperti kuadriliun, dan seterusnya.
+        $hasilBagi = floor(bcdiv($angkaAsli, '1000000000000'));
+        $hasilMod = bcmod($angkaAsli, '1000000000000');
+
+        return rtrim(sprintf('%s triliun %s',
+            static::terjemahkanAngka($hasilBagi),
+            static::terjemahkanAngka($hasilMod)
+        ));
+    }
+
+    /**
+     * Terjemahkan setiap angka menjadi bilangan dalam
+     * Bahasa Indonesia tanpa perlu mengindahkan satuan.
+     *
+     * @param float $angka
+     * @return string
+     */
+    public function terjemahkanPerAngka($angka)
+    {
+        $bilangan = $this->bilangan;
+        $bilangan[0] = 'nol';
+
+        $terbilang = [];
+        $length = strlen($angka);
+
+        for ($i=0; $i<$length; $i++) {
+            $index = (int)$angka{$i};
+            $terbilang[] = $bilangan[$index];
+        }
+
+        return implode(' ', $terbilang);
+    }
+
+    /**
+     * Shortcut untuk method terbilang()
+     *
+     * @param float $angka Angka yang akan diterjemahkan
+     * @return string
+     */
+    public function t($angka)
+    {
+        return $this->terbilang($angka);
+    }
+
+    /**
+     * Magic setter untuk attribute private/protected
+     *
+     * @param string $attr
+     * @param mixed $nilai
+     */
+    public function __set($attr, $nilai)
+    {
+        if (property_exists($this, $attr)) {
+            $this->{$attr} = $nilai;
+        }
+    }
+}
+
+class TerbilangException extends \Exception {}
---
+++ tests/TerbilangTest.php
@@ -0,0 +1,239 @@
+<?php
+namespace RioAstamal\AngkaTerbilang\Test;
+
+use PHPUnit\Framework\TestCase;
+use RioAstamal\AngkaTerbilang\Terbilang;
+
+class TerbilangTest extends TestCase
+{
+    protected $terbilang = null;
+
+    public function setUp()
+    {
+        $this->terbilang = new Terbilang();
+    }
+
+    public function tearDown()
+    {
+        $this->terbilang = null;
+    }
+
+    public function testTerbilangKurangDari12()
+    {
+        $terbilang = new Terbilang();
+        $satuan = [
+            0 => 'nol',
+            1 => 'satu',
+            2 => 'dua',
+            3 => 'tiga',
+            4 => 'empat',
+            5 => 'lima',
+            6 => 'enam',
+            7 => 'tujuh',
+            8 => 'delapan',
+            9 => 'sembilan',
+            10 => 'sepuluh',
+            11 => 'sebelas'
+        ];
+        foreach ($satuan as $angka => $bilangan) {
+            $this->assertEquals($bilangan, $this->terbilang->terbilang($angka));
+        }
+    }
+
+    public function testTerbilangBelasan()
+    {
+        $belasan = [
+            12 => 'dua belas',
+            13 => 'tiga belas',
+            17 => 'tujuh belas',
+            19 => 'sembilan belas'
+        ];
+        foreach ($belasan as $angka => $bilangan) {
+            $this->assertEquals($bilangan, $this->terbilang->terbilang($angka));
+        }
+    }
+
+    public function testTerbilangPuluhan()
+    {
+        $puluhan = [
+            20 => 'dua puluh',
+            21 => 'dua puluh satu',
+            87 => 'delapan puluh tujuh',
+            99 => 'sembilan puluh sembilan'
+        ];
+        foreach ($puluhan as $angka => $bilangan) {
+            $this->assertEquals($bilangan, $this->terbilang->terbilang($angka));
+        }
+    }
+
+    public function testTerbilangRatusan()
+    {
+        $ratusan = [
+            100 => 'seratus',
+            105 => 'seratus lima',
+            111 => 'seratus sebelas',
+            119 => 'seratus sembilan belas',
+            187 => 'seratus delapan puluh tujuh',
+            199 => 'seratus sembilan puluh sembilan',
+            200 => 'dua ratus',
+            211 => 'dua ratus sebelas',
+            222 => 'dua ratus dua puluh dua',
+            999 => 'sembilan ratus sembilan puluh sembilan',
+        ];
+        foreach ($ratusan as $angka => $bilangan) {
+            $this->assertEquals($bilangan, $this->terbilang->terbilang($angka));
+        }
+    }
+
+    public function testTerbilangRibuan()
+    {
+        $ribuan = [
+            1000 => 'seribu',
+            1011 => 'seribu sebelas',
+            1111 => 'seribu seratus sebelas',
+            1119 => 'seribu seratus sembilan belas',
+            1187 => 'seribu seratus delapan puluh tujuh',
+            1199 => 'seribu seratus sembilan puluh sembilan',
+            2200 => 'dua ribu dua ratus',
+            2211 => 'dua ribu dua ratus sebelas',
+            4222 => 'empat ribu dua ratus dua puluh dua',
+            9999 => 'sembilan ribu sembilan ratus sembilan puluh sembilan',
+            10111 => 'sepuluh ribu seratus sebelas',
+            78521 => 'tujuh puluh delapan ribu lima ratus dua puluh satu',
+            99999 => 'sembilan puluh sembilan ribu sembilan ratus sembilan puluh sembilan',
+            100012 => 'seratus ribu dua belas',
+            999999 => 'sembilan ratus sembilan puluh sembilan ribu sembilan ratus sembilan puluh sembilan'
+        ];
+        foreach ($ribuan as $angka => $bilangan) {
+            $this->assertEquals($bilangan, $this->terbilang->terbilang($angka));
+        }
+    }
+
+    public function testTerbilangJutaan()
+    {
+        $jutaan = [
+            1000000 => 'satu juta',
+            1000011 => 'satu juta sebelas',
+            3250000 => 'tiga juta dua ratus lima puluh ribu',
+            9999999 => 'sembilan juta sembilan ratus sembilan puluh sembilan ribu sembilan ratus sembilan puluh sembilan',
+            11000000 => 'sebelas juta',
+            12000050 => 'dua belas juta lima puluh',
+            99999999 => 'sembilan puluh sembilan juta sembilan ratus sembilan puluh sembilan ribu sembilan ratus sembilan puluh sembilan',
+            100000000 => 'seratus juta',
+            100000001 => 'seratus juta satu',
+            787654321 => 'tujuh ratus delapan puluh tujuh juta enam ratus lima puluh empat ribu tiga ratus dua puluh satu',
+            999999999 => 'sembilan ratus sembilan puluh sembilan juta sembilan ratus sembilan puluh sembilan ribu sembilan ratus sembilan puluh sembilan'
+        ];
+        foreach ($jutaan as $angka => $bilangan) {
+            $this->assertEquals($bilangan, $this->terbilang->terbilang($angka));
+        }
+    }
+
+    public function testTerbilangMilyaran()
+    {
+        $milyaran = [
+            '1000000000' => 'satu milyar',
+            '1000000001' => 'satu milyar satu',
+            '1000000010' => 'satu milyar sepuluh',
+            '3000000211' => 'tiga milyar dua ratus sebelas',
+            '9999999999' => 'sembilan milyar sembilan ratus sembilan puluh sembilan juta sembilan ratus sembilan puluh sembilan ribu sembilan ratus sembilan puluh sembilan',
+            '11000000000' => 'sebelas milyar',
+            '35200000000' => 'tiga puluh lima milyar dua ratus juta',
+            '99999999999' => 'sembilan puluh sembilan milyar sembilan ratus sembilan puluh sembilan juta sembilan ratus sembilan puluh sembilan ribu sembilan ratus sembilan puluh sembilan',
+            '125000005000' => 'seratus dua puluh lima milyar lima ribu',
+            '999999999999' => 'sembilan ratus sembilan puluh sembilan milyar sembilan ratus sembilan puluh sembilan juta sembilan ratus sembilan puluh sembilan ribu sembilan ratus sembilan puluh sembilan'
+        ];
+        foreach ($milyaran as $angka => $bilangan) {
+            $this->assertEquals($bilangan, $this->terbilang->terbilang($angka));
+        }
+    }
+
+    public function testTerbilangTriliunan()
+    {
+        $triliunan = [
+            '1000000000000' => 'satu triliun',
+            '1000000000111' => 'satu triliun seratus sebelas',
+            '1000001111111' => 'satu triliun satu juta seratus sebelas ribu seratus sebelas',
+            '9999999999999' => 'sembilan triliun sembilan ratus sembilan puluh sembilan milyar sembilan ratus sembilan puluh sembilan juta sembilan ratus sembilan puluh sembilan ribu sembilan ratus sembilan puluh sembilan',
+            '70000360000000' => 'tujuh puluh triliun tiga ratus enam puluh juta',
+            '100000000000000' => 'seratus triliun',
+            '562000000000321' => 'lima ratus enam puluh dua triliun tiga ratus dua puluh satu',
+            '999999999999999' => 'sembilan ratus sembilan puluh sembilan triliun sembilan ratus sembilan puluh sembilan milyar sembilan ratus sembilan puluh sembilan juta sembilan ratus sembilan puluh sembilan ribu sembilan ratus sembilan puluh sembilan',
+            '1000000000000000' => 'seribu triliun',
+            '3500000000000000' => 'tiga ribu lima ratus triliun',
+            '11000000001000222' => 'sebelas ribu triliun satu juta dua ratus dua puluh dua'
+        ];
+        foreach ($triliunan as $angka => $bilangan) {
+            $this->assertEquals($bilangan, $this->terbilang->terbilang($angka));
+        }
+    }
+
+    public function testTerbilangDenganKoma()
+    {
+        $angkaKoma = [
+            '11.568' => 'sebelas koma lima enam delapan',
+            '3.141592' => 'tiga koma satu empat satu lima sembilan dua',
+            '1000.051' => 'seribu koma nol lima satu',
+            '70.350' => 'tujuh puluh koma tiga lima nol',
+            '0.0003' => 'nol koma nol nol nol tiga',
+            '100000000000000.874950' => 'seratus triliun koma delapan tujuh empat sembilan lima nol'
+        ];
+        foreach ($angkaKoma as $angka => $bilangan) {
+            $this->assertEquals($bilangan, $this->terbilang->terbilang($angka));
+        }
+    }
+
+    /**
+     * @expectedException RioAstamal\AngkaTerbilang\TerbilangException
+     * @expectedExceptionMessage ERROR: Angka tidak dapat dikenali
+     */
+    public function testAngkaKomaGanda()
+    {
+        $angka = '123.22.22';
+        $this->terbilang->terbilang($angka);
+    }
+
+    public function testPemisahRibuanDenganBeberapaPemisah()
+    {
+        $pemisahRibuan = [
+            '1 100' => 'seribu seratus',
+            '1 011 000' => 'satu juta sebelas ribu',
+            '1,100' => 'seribu seratus',
+            '1,011,000' => 'satu juta sebelas ribu',
+            '2,000,000.56' => 'dua juta koma lima enam'
+        ];
+        foreach ($pemisahRibuan as $angka => $bilangan) {
+            $this->assertEquals($bilangan, $this->terbilang->terbilang($angka));
+        }
+    }
+
+    public function testPemisahDesimalBukanTitik()
+    {
+        $this->terbilang->pemisahDesimal = ',';
+        $pemisahRibuan = [
+            '0,005' => 'nol koma nol nol lima',
+            '2500,75' => 'dua ribu lima ratus koma tujuh lima',
+            '2.500,75' => 'dua ribu lima ratus koma tujuh lima',
+            '1.000.750,999' => 'satu juta tujuh ratus lima puluh koma sembilan sembilan sembilan',
+            '1 000 750,999' => 'satu juta tujuh ratus lima puluh koma sembilan sembilan sembilan',
+        ];
+        foreach ($pemisahRibuan as $angka => $bilangan) {
+            $this->assertEquals($bilangan, $this->terbilang->terbilang($angka));
+        }
+    }
+
+    public function testShortcutMethod()
+    {
+        $this->terbilang->pemisahDesimal = ',';
+        $pemisahRibuan = [
+            '0,005' => 'nol koma nol nol lima',
+            '2500,75' => 'dua ribu lima ratus koma tujuh lima',
+            '2.500,75' => 'dua ribu lima ratus koma tujuh lima',
+            '1.000.750,999' => 'satu juta tujuh ratus lima puluh koma sembilan sembilan sembilan',
+            '1 000 750,999' => 'satu juta tujuh ratus lima puluh koma sembilan sembilan sembilan',
+        ];
+        foreach ($pemisahRibuan as $angka => $bilangan) {
+            $this->assertEquals($bilangan, $this->terbilang->t($angka));
+        }
+    }
+}
